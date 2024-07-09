10768708064f520b996aa93e51bb8c3c75f6ec41
JmoulderYell <josh.moulder@yell.com>
2024-07-09 16:10:37 +0100

    Changes to hello

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import click
 
 @click.command()
-@click.option('--name', prompt='Your name', help='The person to greet')
+@click.argument('name', default='you')
+
 def hello(name):
-    """Greet the person specified by --name."""
+    """Greet the person specified by name."""
     click.echo(f"Hello {name}!")
