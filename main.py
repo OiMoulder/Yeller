9b4d5f3b60f1412ef5ca1348c2e37a53191b5877
JmoulderYell <josh.moulder@yell.com>
2024-07-09 14:20:26 +0100

    Changes

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""yeller
+
+Author:
+    Josh Moulder <josh.moulder12@gmail.com>
+"""
+
+import click
+from hello import hello  # Import the hello command
+
+# Set context settings for click
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+# Version number
+version_num = "0.0.1"
+
+# Define the main command group
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version=version_num, prog_name="Yeller", help="Show the version and exit.")
+def cli():
+    """Yeller is the Yell CLI toolset -- a single command with multiple subcommands for automating repeatable tasks."""
+    pass
+
+# Add the hello command to the main command group
+cli.add_command(hello)
+
+if __name__ == '__main__':
+    cli()
